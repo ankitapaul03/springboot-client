e7524fa71ba29609e9156b692c0f2c2c708f9dc8
Ankita Paul <ankitapaul1808@gmail.com>
2022-07-19 00:49:36 +0530

    Implemented update and details viewing functionalities

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-update-employee',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  employee!: Employee;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,private router: Router,
+    private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.employee = new Employee();
+
+    this.id = this.route.snapshot.params['id'];
+    
+    this.employeeService.getEmployee(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.employee = data;
+      }, error => console.log(error));
   }
 
+  updateEmployee() {
+    this.employeeService.updateEmployee(this.id, this.employee)
+      .subscribe(data => {
+        console.log(data);
+        this.employee = new Employee();
+        this.gotoList();
+      }, error => console.log(error));
+  }
+
+  onSubmit() {
+    this.updateEmployee();    
+  }
+
+  gotoList() {
+    this.router.navigate(['/employees']);
+  }
 }
