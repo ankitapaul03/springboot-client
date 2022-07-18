76ceac0aa0268fe4f4010821786812d8a4eb7d03
Ankita Paul <ankitapaul1808@gmail.com>
2022-07-19 01:24:34 +0530

    edit in title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SpringBootClient-Angular Crud Application';
+  title = 'SpringBootClient-Angular CRUD Application';
 }
